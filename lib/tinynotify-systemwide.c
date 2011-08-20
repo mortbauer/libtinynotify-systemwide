0eb82817cfc9817d7895c575aa27f17c144e8fb3
Michał Górny <mgorny@gentoo.org>
2011-08-20 22:39:31 +0200

    Provide the base for libtinynotify-systemwide.

---
+++ lib/tinynotify-systemwide.c
@@ -0,0 +1,8 @@
+/* libtinynotify
+ * (c) 2011 Michał Górny
+ * 2-clause BSD-licensed
+ */
+
+#include "config.h"
+#include "tinynotify-systemwide.h"
+#include <tinynotify.h>
---
+++ lib/tinynotify-systemwide.h
@@ -0,0 +1,20 @@
+/* libtinynotify
+ * (c) 2011 Michał Górny
+ * 2-clause BSD-licensed
+ */
+
+#ifndef _TINYNOTIFY_CLI_H
+#define _TINYNOTIFY_CLI_H
+
+#include <tinynotify.h>
+
+/**
+ * SECTION: systemwide
+ * @short_description: API to handle system-wide notifications
+ * @include: tinynotify-systemwide.h
+ *
+ * The libtinynotify-systemwide helper library provides the routines to send
+ * notifications system-wide, i.e. on all D-Bus session buses in the system.
+ */
+
+#endif
